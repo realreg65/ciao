ed170e0a0a5c58d237b4f57bb51d733d78c21030
mireginato <mireginato@Lab06_Cattedra1.itseinaudi.local>
2016-02-06 10:53:15 +0100

    prima prova

---
+++ ciao/src/ciao/Ciao.java
@@ -0,0 +1,63 @@
+package ciao;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class Ciao {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Ciao window = new Ciao();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		Button btnSaluti = new Button(shell, SWT.NONE);
+		btnSaluti.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				MessageDialog.openInformation(shell, "titolo", "Ciao mondo");
+				
+			}
+		});
+		btnSaluti.setBounds(150, 109, 135, 25);
+		btnSaluti.setText("Saluti");
+
+	}
+}
